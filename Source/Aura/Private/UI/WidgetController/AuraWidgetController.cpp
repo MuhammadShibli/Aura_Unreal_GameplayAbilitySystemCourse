2238e5e43aa6c3b0c8367055a30bfb40c426b61e
Shibli <shibli.mansuri@gmail.com>
2024-09-28 15:02:05 +0500

    Add Widget and WIdgetControllerClass

---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright M.Shibli Mansuri
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
---
+++ Source/Aura/Private/UI/Widgets/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright M.Shibli Mansuri
+
+
+#include "UI/Widgets/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
